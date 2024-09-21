c35b7813e81cd8b533208b5fbe8b14bd7cb5c8c8
HectorGuerra004 <hectorsantiagoguerra004@gmail.com>
2024-09-20 21:32:19 -0400

    Add files via upload

---
+++ components/navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div class="container-fluid">
+            <!-- Logo a la izquierda -->
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/img/logo-removebg-preview.png" alt="Logo" width="98" height="60"> <b>GameOn Fc</b>
+            </a>
+
+            <!-- Toggler for mobile view -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Opciones alineadas a la derecha -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto mx-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php"><b>INICIO</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a sty class="nav-link" href="registrarse.php"><b>REGISTRO</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="IniciarSesion.php"><b>ACCEDER</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="torneos.php"><b>TORNEOS</b></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
